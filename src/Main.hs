fcbcac51f75696aeafa0a2315b0e577df5568c05
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2019-06-07 16:49:09 +0300

    rewrite interop from sockets to stdin/stdout

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,21 @@
 {-# LANGUAGE MultiWayIf #-}
 module Main where
 
-import System.Environment
-
+import System.Environment (getArgs)
+import System.Posix.Process (getProcessID)
 import Server (server, client)
 
 main :: IO ()
 main = do
-    [mode] <- getArgs
-    if | mode == "server" -> server
-       | mode == "client" -> client
-       | otherwise        -> putStrLn "Specify the mode (server or client)!"
+    pid <- getProcessID
+    putStrLn $ "Started with pid " ++ show pid
+    args <- getArgs
+    putStrLn $ "Args taken: " ++ show args
+    if | "--persistent-worker" `elem` args
+         -> server
+       | "client" `elem` args
+         -> client (read (args !! 1) :: Int)
+       | otherwise
+         -> putStrLn $
+            "Compiler started in a non-persistent mode!" ++
+            "Use --persistent-worker. Exit."
--- src/Server.hs
+++ src/Server.hs
@@ -1,15 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Server (server, client) where
 
-{- Networking -}
-import Control.Concurrent (forkFinally)
-import qualified Control.Exception as E
-import Control.Monad (unless, forever, void)
-import qualified Data.ByteString as S
-import qualified Data.ByteString.Char8 as C
-import Network.Socket
-import Network.Socket.ByteString (recv, sendAll)
-
 {- ProtoBuf -}
 import Proto.Worker as W
 import Proto.Worker_Fields as W
@@ -18,48 +9,19 @@ import Data.ProtoLens.Encoding (buildMessage, decodeMessage)
 import Data.ProtoLens.Encoding.Bytes (runBuilder)
 import Lens.Micro
 
+import qualified Data.ByteString as S
+
+import Control.Monad (forever)
 import Data.Either (fromRight)
-
-port :: Int
-port = 44444
-
-max_bytes_msg :: Int
-max_bytes_msg = 4096
+import System.IO
 
 server :: IO ()
-server = withSocketsDo $ do
-    addr <- resolve port
-    E.bracket (open addr) close loop
-  where
-    resolve port = do
-        let hints = defaultHints {
-                addrFlags = [AI_PASSIVE]
-              , addrSocketType = Stream
-              }
-        addr:_ <- getAddrInfo (Just hints) Nothing (Just . show $ port)
-        return addr
-    open addr = do
-        sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-        setSocketOption sock ReuseAddr 1
-        -- If the prefork technique is not used,
-        -- set CloseOnExec for the security reasons.
-        withFdSocket sock $ \fd -> do
-          setCloseOnExecIfNeeded fd
-          bind sock (addrAddress addr)
-          listen sock 10
-          return sock
-    loop sock = forever $ do
-        (conn, peer) <- accept sock
-        putStrLn $ "Connection from " ++ show peer
-        void $ forkFinally (talk conn) (\_ -> close conn)
-    talk conn = do
-        msg <- recv conn max_bytes_msg
-        unless (S.null msg) $ do
-          let req = (fromRight undefined $ decodeMessage msg) :: W.WorkRequest
-          putStrLn $ "Received: " ++ (show req)
-          let resp = processRequest req
-          sendAll conn . runBuilder $ buildMessage resp
-          talk conn
+server = forever $ do
+    msg <- S.hGetContents stdin
+    let req = (fromRight undefined $ decodeMessage msg) :: W.WorkRequest
+    putStrLn $ "Received: " ++ show req
+    let resp = processRequest req
+    S.hPut stdout . runBuilder . buildMessage $ resp
 
 processRequest :: W.WorkRequest -> W.WorkResponse
 processRequest _ = sampleResponse
@@ -69,25 +31,29 @@ sampleResponse = defMessage
     & W.exitCode .~ 0
     & W.output   .~ "All good"
 
-client :: IO ()
-client = withSocketsDo $ do
-    addr <- resolve "127.0.0.1" port
-    E.bracket (open addr) close talk
-  where
-    resolve host port = do
-        let hints = defaultHints { addrSocketType = Stream }
-        addr:_ <- getAddrInfo (Just hints) (Just host) (Just . show $ port)
-        return addr
-    open addr = do
-        sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-        connect sock $ addrAddress addr
-        return sock
-    talk sock = do
-        sendAll sock . runBuilder $ buildMessage sampleReq
-        msg <- recv sock max_bytes_msg
-        putStr "Received: "
-        let resp = (fromRight undefined $ decodeMessage msg) :: W.WorkResponse
-        putStrLn (show resp)
+serverDesc :: Int -> Int -> String
+serverDesc pid fd = 
+  "/proc/" ++ show pid ++ "/fd/" ++ show fd
+
+_STDIN :: Int
+_STDIN = 0
+
+_STDOUT :: Int
+_STDOUT = 1
+
+client :: Int -> IO ()
+client server_pid = do
+    putStrLn "Client started"
+    hSend <- openBinaryFile
+        (serverDesc server_pid _STDIN)
+        WriteMode
+    hRecv <- openBinaryFile
+        (serverDesc server_pid _STDOUT)
+        ReadMode
+    S.hPut hSend . runBuilder . buildMessage $ sampleReq
+    msg <- S.hGetContents hRecv
+    let resp = (fromRight undefined $ decodeMessage msg) :: W.WorkResponse
+    putStrLn $ "Received: " ++ showMessage resp
 
 sampleReq :: W.WorkRequest
 sampleReq = defMessage
